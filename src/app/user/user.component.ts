061b66738dc2b09a255b677c9359eadc6e64e1bf
Stefan Jaroni <sjaroni@outlook.de>
2024-03-23 21:53:05 +0100

    feat: add button for new user

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [],
+  imports: [MatIcon, MatButtonModule, MatTooltipModule],
   templateUrl: './user.component.html',
-  styleUrl: './user.component.scss'
+  styleUrl: './user.component.scss',
 })
-export class UserComponent {
-
-}
+export class UserComponent {}
