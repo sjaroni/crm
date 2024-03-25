dfec51c1e33c13a7c7d66b8006a409347a9920f8
Stefan Jaroni <sjaroni@outlook.de>
2024-03-25 20:04:32 +0100

    style: add user table prototype

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -5,24 +5,27 @@ import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { DialogAddUserComponent } from '../dialog-add-user/dialog-add-user.component';
 import { User } from '../../models/user.class';
-
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [MatIcon, MatButtonModule, MatTooltipModule, MatDialogModule],
+  imports: [
+    MatIcon, 
+    MatButtonModule, 
+    MatTooltipModule, 
+    MatDialogModule,
+    MatCardModule
+  ],
   templateUrl: './user.component.html',
   styleUrl: './user.component.scss',
 })
 export class UserComponent {
   user = new User();
-    
+
   constructor(public dialog: MatDialog) {}
 
   openDialog(): void {
-    this.dialog.open(DialogAddUserComponent, {
-      
-    });
-    
+    this.dialog.open(DialogAddUserComponent, {});
   }
 }
